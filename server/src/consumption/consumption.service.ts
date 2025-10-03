2ff82664e0d21d6d9922e00ac83cf47c14cae18a
Xavier To <xaviertonneller6@outlook.fr>
2025-10-04 00:02:21 +0200

    [FEAT] little mistake

--- server/src/consumption/consumption.service.ts
+++ server/src/consumption/consumption.service.ts
@@ -97,7 +97,7 @@ export class ConsumptionService {
 
     const baseQb = this.repository
       .createQueryBuilder('record')
-      .where('record.userId = :userId', { userId });
+      .where('record.user_id = :userId', { userId });
     this.applyWindow(baseQb, from, to);
 
     const total = await baseQb.clone().getCount();
